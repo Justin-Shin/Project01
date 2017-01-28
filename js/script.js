1e8e38052e3ae272e5fd9d1938e5b563b1f315a5
Justin Shin <jjs3b@virginia.edu>
2017-01-27 22:10:11 -0500

    trying to get into the habit of committing more often. setting up the look of the game, no logic yet. getting the part I find the most boring out of the way.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var originalValues;
+
+// $('.button').on('mousedown',function(){
+//   originalValues = $(this).css('box-shadow')
+//   $(this).css('box-shadow',' 0 0 0 0');
+//   $(this).css('margin','0vh')
+// })
+// $('.button').on('mouseup',function(){
+//   $(this).css('box-shadow',originalValues);
+//   $(this).css('margin','1vh')
+// })
